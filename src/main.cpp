f88a5c4bd7552b11b9cd7c0bb818c53ff2715249
djded <ddjded@hotmail.com>
2026-06-03 16:02:41 +0200

    Configure Bluetooth audio over I2S

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <Arduino.h>
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
 
-constexpr uint8_t LED_PIN = 2;
-constexpr unsigned long BLINK_INTERVAL_MS = 500;
+constexpr int I2S_BCK_PIN = 25;
+constexpr int I2S_WS_PIN = 26;
+constexpr int I2S_DATA_PIN = 22;
+
+I2SStream i2s;
+BluetoothA2DPSink a2dpSink(i2s);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting blink...");
-  pinMode(LED_PIN, OUTPUT);
+  Serial.println("Starting Bluetooth A2DP sink...");
+
+  auto cfg = i2s.defaultConfig();
+  cfg.pin_bck = I2S_BCK_PIN;
+  cfg.pin_ws = I2S_WS_PIN;
+  cfg.pin_data = I2S_DATA_PIN;
+  i2s.begin(cfg);
+
+  a2dpSink.start("Fabia BT Audio");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(BLINK_INTERVAL_MS);
-  digitalWrite(LED_PIN, LOW);
-  delay(BLINK_INTERVAL_MS);
 }
